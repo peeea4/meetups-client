cf33516551ef30c0be21d0be71d350c7200b710c
Nikita Gabetc <nikita.gabetc@gmail.com>
2024-02-04 19:02:27 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { AppRouter } from './components/AppRouter/AppRouter';
 import { store } from './store/store';
@@ -9,8 +9,8 @@ import GlobalStyles from './styles/global';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <GlobalStyles />
-        <BrowserRouter>
+        <HashRouter>
             <AppRouter />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
